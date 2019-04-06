3cd9bfcf471ab6a6ab9bdabf94ec69c2488ac0e9
I506638 <rahul.ramanathan@sap.com>
2019-04-06 23:17:14 +0530

    Combined encrypt decrypt into one file

---
+++ caesar_encr_decr.py
@@ -0,0 +1,32 @@
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def encrypt(text, key, mode):
+    result = ""
+    # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+
+        if mode is "E":
+            # Encrypt uppercase characters in plain text
+            if char in LETTERS:
+                result = result + chr((ord(char) + key - 65) % 26 + 65)
+            # Encrypt lowercase characters in plain text
+            else:
+                result = result + char
+        elif mode is "D":
+            # Decrypt uppercase characters in plain text
+            if char in LETTERS:
+                result = result + chr((ord(char) - key - 65) % 26 + 65)
+            # Decrypt lowercase characters in plain text
+            else:
+                result = result + char
+        else:
+            result = "Invalid Mode"
+    return result
+
+# check the above function
+text = "WYGGIWWJEGXSVW".upper()
+key = 4
+
+print("Plain Text : " + text)
+print("Shift pattern : " + str(key))
+print("Cipher: " + encrypt(text, key, "D"))
